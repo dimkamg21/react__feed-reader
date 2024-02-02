40693637ef8b091ec21c83fc96bd2dd16ea06668
Dmytro <dmytro2105@gmail.com>
2024-02-02 22:27:41 +0200

    remove rss

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/react__feed-reader/',
+  base: '/react__feed-reader/',
   plugins: [react()],
 })
