34227afa5d1fe58e003f00dad98eea6dcc5480c3
Dmytro <dmytro2105@gmail.com>
2024-02-02 22:08:46 +0200

    final version

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { RequireAuth } from "./components/Auth/RequireAuth";
 import { AuthProvider } from "./components/Auth/AuthContext";
-import { LoginPage } from "./pages/LoginPage";
+import { NotFoundPage } from "./pages/NotFoundPage.tsx";
+import { Header } from "./components/Header/Header.tsx";
 import { HomePage } from "./pages/HomePage.tsx";
-import {Header} from "./components/Header/Header.tsx";
+import { LoginPage } from "./pages/LoginPage";
 import "./App.css";
 
 export const App = () => (
@@ -18,6 +19,8 @@ export const App = () => (
             <Route path="/" element={<HomePage />} />
           </Route>
         </Route>
+
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </AuthProvider>
   </BrowserRouter>
